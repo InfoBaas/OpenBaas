28107db4fd9c2bc79f81cac646b764e5f5d4d6fc
pponte <paulo.ponte@infosistema.com>
2013-10-21 17:25:37 +0100

    Geolocation

---
+++ src/main/java/infosistema/openbaas/middleLayer/MediaMiddleLayer.java
@@ -0,0 +1,32 @@
+package infosistema.openbaas.middleLayer;
+
+import infosistema.openbaas.dataaccess.models.Model;
+import infosistema.openbaas.model.media.image.Image;
+import infosistema.openbaas.model.media.image.ImageInterface;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public abstract class MediaMiddleLayer {
+
+	// *** MEMBERS *** ///
+
+	protected Model model;
+	
+	// *** CREATE *** ///
+	
+	public int uploadImage() {
+		return 0;
+	}
+
+	// *** UPDATE *** ///
+	
+	// *** DELETE *** ///
+	
+	// *** GET LIST *** ///
+	
+	// *** GET *** ///
+	
+	// *** OTHERS *** ///
+	
+}
