f550cf48687638c95a2a909410b8a1285c16bb93
jmarcos <jmarcos@ubuntu>
2014-05-03 16:22:47 +0100

    novos logs

--- src/main/java/infosistema/openbaas/utils/ApplePushNotifications.java
+++ src/main/java/infosistema/openbaas/utils/ApplePushNotifications.java
@@ -15,12 +15,12 @@ public class ApplePushNotifications {
 	
 	
 	public static void pushCombineNotification(String alertText, int badge, String keystore, String password, Boolean production, Object devices) throws CommunicationException, KeystoreException {
-		/*Log.error("", "", "pushCombineNotification", "********pushCombineNotification ###### alert:"+alertText);
+		Log.error("", "", "pushCombineNotification", "********pushCombineNotification ###### alert:"+alertText);
 		List<Device> devs = (List<Device>)devices;
 		Iterator<Device> it = devs.iterator();
 		while(it.hasNext()){
 			Log.error("", "", "push", "push0:" +"keystore:" +keystore+" - password:"+password+" - production:"+production+" - devices token:"+ it.next().getToken());
-		}*/
+		}
 		List<PushedNotification> notifications = Push.combined(alertText, badge, "default", keystore, password, production, devices);
 		printPushedNotifications(notifications);
 	}
@@ -33,11 +33,11 @@ public class ApplePushNotifications {
 	}
 	
 	public static void pushBadgeService(int badge,String keystore, String password, Boolean production, Object devices) throws CommunicationException, KeystoreException {
-		/*List<Device> devs = (List<Device>)devices;
+		List<Device> devs = (List<Device>)devices;
 		Iterator<Device> it = devs.iterator();
 		while(it.hasNext()){
 			Log.error("", "", "pushBadge", "pushBadge:" +"keystore:" +keystore+" - password:"+password+" - production:"+production+" - devices token:"+ it.next().getToken());
-		}*/
+		}
 		List<PushedNotification> notifications = Push.badge(badge, keystore, password, production, devices);
 		printPushedNotifications(notifications);
 		
@@ -58,7 +58,7 @@ public class ApplePushNotifications {
 		} else if (successful == 0 && failed > 0) {
 			printPushedNotifications("All notifications failed (" + failedNotifications.size() + "):", failedNotifications);
 		} else if (successful == 0 && failed == 0) {
-			Log.info("","", "printPushedNotifications ->", "No notifications could be sent, probably because of a critical error");
+			Log.error("","", "printPushedNotifications ->", "No notifications could be sent, probably because of a critical error");
 		} else {
 			printPushedNotifications("Some notifications failed (" + failedNotifications.size() + "):", failedNotifications);
 			printPushedNotifications("Others succeeded (" + successfulNotifications.size() + "):", successfulNotifications);
@@ -73,9 +73,9 @@ public class ApplePushNotifications {
 	public static void printPushedNotifications(String description, List<PushedNotification> notifications) {
 		for (PushedNotification notification : notifications) {
 			try {
-				Log.info("","", "printPushedNotifications desc->", description +" Notification:"+ notification.toString());
+				Log.error("","", "printPushedNotifications desc->", description +" Notification:"+ notification.toString());
 			} catch (Exception e) {
-				e.printStackTrace();
+				Log.error("","", "printPushedNotifications desc->","Error in Notification:"+ notification.toString()+ e.toString());
 			}
 		}
 	}
