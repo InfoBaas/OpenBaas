1d48939011a140ae4d387b9e1aacfb64a8520f0d
pponte <paulo.ponte@infosistema.com>
2014-11-06 15:48:27 +0000

    Licenses

--- src/main/java/infosistema/openbaas/comunication/connector/IConnector.java
+++ src/main/java/infosistema/openbaas/comunication/connector/IConnector.java
@@ -1,3 +1,22 @@
+/*****************************************************************************************
+Infosistema - OpenBaas
+Copyright(C) 2002-2014 Infosistema, S.A.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Affero General Public License for more details.
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+www.infosistema.com
+info@openbaas.com
+Av. José Gomes Ferreira, 11 3rd floor, s.34
+Miraflores
+1495-139 Algés Portugal
+****************************************************************************************/
 package infosistema.openbaas.comunication.connector;
 
 import infosistema.openbaas.comunication.message.Message;
